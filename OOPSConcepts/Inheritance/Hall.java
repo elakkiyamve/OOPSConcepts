878aa6e49d237977f8fd943427a634c3423c7f98
Elakkiya aravind <94050446+elakkiyamve@users.noreply.github.com>
2021-11-20 16:49:49 +0530

    Inheritance

---
+++ OOPSConcepts/Inheritance/Hall.java
@@ -0,0 +1,11 @@
+package com.elakkiya.OOPSConcepts.Inheritance;
+
+
+public class Hall extends Home{
+
+
+    public Hall(int noOfLights, String roomName) {
+        super(noOfLights, roomName);
+    }
+}
+
---
+++ OOPSConcepts/Inheritance/MasterRoom.java
@@ -0,0 +1,8 @@
+package com.elakkiya.OOPSConcepts.Inheritance;
+
+public class MasterRoom extends Home{
+
+    public MasterRoom(int noOfLights, String roomName) {
+        super(noOfLights, roomName);
+    }
+}
